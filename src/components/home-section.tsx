97802995ab914a4c2c54310e14456da012a9d6bf
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-04-03 18:09:59 -0300

    fix: remove sprout and change subtitle

--- src/components/home-section.tsx
+++ src/components/home-section.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { SproutIcon } from "lucide-react";
-
 export function HomeSection() {
   return (
     <section
       id="home"
       className="w-full items-center justify-center text-white bg-[url(/background.jpg)] h-[85vh] bg-center bg-black/70 bg-no-repeat bg-cover bg-blend-color bg-opacity-50 border-b flex flex-col"
     >
-      <SproutIcon className="mb-5" size="36" />
       <p className="text-4xl font-bold tracking-wide">Helder Barbosa</p>
       <span className="text-base tracking-tight">
-        Psicoterapeuta & Psicólogo
+        Psicólogo Clínico e Psicanalista
       </span>
     </section>
   );
