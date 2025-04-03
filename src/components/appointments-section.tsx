cd19bb2968e3874a272074344686ac55b309298b
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-04-03 18:19:05 -0300

    fix: appointments layout

--- src/components/appointments-section.tsx
+++ src/components/appointments-section.tsx
@@ -6,97 +6,105 @@ export function AppointmentsSection() {
       id="appointments"
       className="w-full flex flex-col items-center justify-center bg-black pb-20"
     >
-      <h1 className="text-4xl font-bold mt-20">Sobre os atendimentos</h1>
+      <div className="container">
+        <h1 className="text-4xl font-bold mt-20 text-center">
+          Sobre os atendimentos
+        </h1>
 
-      <div className="w-full flex flex-col mb-14 mt-10 items-center justify-center text-white bg-black bg-no-repeat bg-cover bg-blend-color bg-opacity-50">
-        <div className="h-fit w-[95%] flex flex-col gap-10 items-center justify-center text-center">
-          <h3 className="text-2xl font-semibold">
-            Por que as pessoas procuram psicoterapia?
-          </h3>
+        <div className="w-full flex flex-col mb-14 mt-10 items-center justify-center text-white bg-black bg-no-repeat bg-cover bg-blend-color bg-opacity-50">
+          <div className="h-fit flex flex-col gap-10 items-center text-center">
+            <h3 className="text-2xl font-semibold">
+              Por que as pessoas procuram psicoterapia?
+            </h3>
 
-          <p className="text-[16px] font-normal">
-            Como é de se deduzir: as pessoas procuram psicoterapia porque estão
-            em sofrimento intenso (sintomas explícitos) e/ou estão muito
-            insatisfeitas com a própria vida (crises existenciais). De forma
-            genérica é possível expor um pouco desta motivações da seguinte
-            forma:
-          </p>
+            <p className="text-base font-normal">
+              Como é de se deduzir: as pessoas procuram psicoterapia porque
+              estão em sofrimento intenso (sintomas explícitos) e/ou estão muito
+              insatisfeitas com a própria vida (crises existenciais). De forma
+              genérica é possível expor um pouco desta motivações da seguinte
+              forma:
+            </p>
 
-          <ul className="list-disc list-inside flex flex-col items-center justify-center gap-8 w-full">
-            <li className="text-[16px] font-normal w-[90%]">
-              A primeira (sintomas explícitos) é quando a condição está crítica.
-              Estas pessoas se encontram em sofrimentos ou crises intensas que
-              as paralisam e/ou as submetem a variadas limitações no viver
-              cotidiano, como: problemas com ‘auto-estima’, inseguranças
-              limitantes, déficit de atenção, hiperatividade, depressão, manias,
-              ansiedade, persecutoriedade, problemas com sono, com alimentação,
-              com relacionamentos, eficiência, doenças psicossomáticas etc.,
-              etc., etc. Ou seja, condições que incapacitam as funções e
-              necessidades básicas da vida.
-            </li>
+            <ul className="list-disc list-inside flex flex-col text-justify justify-center gap-8 w-full">
+              <li className="text-base font-normal">
+                A primeira (sintomas explícitos) é quando a condição está
+                crítica. Estas pessoas se encontram em sofrimentos ou crises
+                intensas que as paralisam e/ou as submetem a variadas limitações
+                no viver cotidiano, como: problemas com ‘auto-estima’,
+                inseguranças limitantes, déficit de atenção, hiperatividade,
+                depressão, manias, ansiedade, persecutoriedade, problemas com
+                sono, com alimentação, com relacionamentos, eficiência, doenças
+                psicossomáticas etc., etc., etc. Ou seja, condições que
+                incapacitam as funções e necessidades básicas da vida.
+              </li>
 
-            <li className="text-[16px] font-normal w-[90%]">
-              Sobre as crises existenciais, as observamos através de queixas de
-              diminuição dos ânimos com a vida, os “tanto faz”, apáticos; não
-              necessariamente os depressivos – esta queixa é comum em pessoas
-              que fazem apenas uso de medicamentos psiquiátricos por anos;
-              relatam que os medicamentos realmente diminuíram e/ou eliminaram o
-              sofrimento, porém, diminuiu também o ‘sabor da vida’. Estas
-              pessoas, sejam elas medicalizadas ou não, costumam buscar ajuda
-              para se reencontrarem consigo mesmas, procuram habitar o próprio
-              Ser. Estão em busca do que faz sentido para si próprio e não
-              apenas manterem-se ajustadas aos padrões. Isto pode parecer
-              simples, mas, na atualidade, muitas pessoas estão alienadas de si
-              próprias – tanto que, não é difícil observarmos, no dia-a-dia,
-              pessoas que sempre culpabilizam os outros por seu infortúnio;
-              nunca tem sua parcela de responsabilidade – e de fato não
-              conseguem reconhecer, pois, ‘não habitam o próprio Ser.’
-            </li>
+              <li className="text-base font-normal">
+                Sobre as crises existenciais, as observamos através de queixas
+                de diminuição dos ânimos com a vida, os “tanto faz”, apáticos;
+                não necessariamente os depressivos – esta queixa é comum em
+                pessoas que fazem apenas uso de medicamentos psiquiátricos por
+                anos; relatam que os medicamentos realmente diminuíram e/ou
+                eliminaram o sofrimento, porém, diminuiu também o ‘sabor da
+                vida’. Estas pessoas, sejam elas medicalizadas ou não, costumam
+                buscar ajuda para se reencontrarem consigo mesmas, procuram
+                habitar o próprio Ser. Estão em busca do que faz sentido para si
+                próprio e não apenas manterem-se ajustadas aos padrões. Isto
+                pode parecer simples, mas, na atualidade, muitas pessoas estão
+                alienadas de si próprias – tanto que, não é difícil observarmos,
+                no dia-a-dia, pessoas que sempre culpabilizam os outros por seu
+                infortúnio; nunca tem sua parcela de responsabilidade – e de
+                fato não conseguem reconhecer, pois, ‘não habitam o próprio
+                Ser.’
+              </li>
 
-            <li className="text-[16px] font-normal w-[90%]">
-              Há também outro perfil de demandas: os que procuram psicoterapia
-              como parte do processo de sua formação psicanalítica; que é
-              indispensável.
-            </li>
-          </ul>
-        </div>
+              <li className="text-base font-normal">
+                Há também outro perfil de demandas: os que procuram psicoterapia
+                como parte do processo de sua formação psicanalítica; que é
+                indispensável.
+              </li>
+            </ul>
+          </div>
 
-        <div className="h-fit w-[95%] flex flex-col mt-10 gap-10 items-center justify-center text-center">
-          <h3 className="text-2xl font-semibold">Como o trabalho acontece?</h3>
+          <div className="h-fit flex flex-col mt-10 gap-10 justify-center text-justify">
+            <h3 className="text-2xl font-semibold text-center">
+              Como o trabalho acontece?
+            </h3>
 
-          <p className="text-[16px] font-normal">
-            De forma muito simples: conversando. E, claro, mantendo um bom
-            relacionamento com empatia e sensibilidade.
-          </p>
+            <p className="text-base font-normal">
+              De forma muito simples: conversando. E, claro, mantendo um bom
+              relacionamento com empatia e sensibilidade.
+            </p>
 
-          <p className="text-[16px] font-normal flex flex-col items-center justify-center gap-2">
-            <i className="mb-1 font-semibold">
-              "Mas, apenas conversando? Posso conversar com um amigo, parente
-              etc."
-            </i>
-            É verdade, é possível conversar com muitas outras pessoas, porém,
-            quando desabafamos, estas oferecem palpites e conselhos (que nem
-            sempre seguem) e nossa vida não se transforma. Passamos anos e anos
-            com as mesmas queixas e desabafos e, pior, ouvindo os mesmos
-            palpites. Enfim, ao invés de tentar explicar de forma exaustiva como
-            o trabalho acontece, vamos de ensaio (e já experimentar um pouco de
-            uma conversa de análise).
-          </p>
+            <p className="text-base font-normal flex flex-col justify-center gap-2">
+              <i className="mb-1 font-semibold">
+                "Mas, apenas conversando? Posso conversar com um amigo, parente
+                etc."
+              </i>
+              É verdade, é possível conversar com muitas outras pessoas, porém,
+              quando desabafamos, estas oferecem palpites e conselhos (que nem
+              sempre seguem) e nossa vida não se transforma. Passamos anos e
+              anos com as mesmas queixas e desabafos e, pior, ouvindo os mesmos
+              palpites. Enfim, ao invés de tentar explicar de forma exaustiva
+              como o trabalho acontece, vamos de ensaio (e já experimentar um
+              pouco de uma conversa de análise).
+            </p>
 
-          <p className="text-[16px] font-normal">
-            Imaginem: uma criança na cozinha imita os mesmos movimentos de sua
-            mãe que prepara o bolo. Manuseia farinha, fermento, cacau, morango,
-            açúcar, água, etc. Se não estiver sob orientação, a criança faz
-            apenas sujeira com os ingredientes. A mãe os transforma num bolo,
-            pois, domina a ‘alquimia’ - as medidas e o tempo adequado para a
-            transformação. Os palpiteiros e conselheiros são como essa criança,
-            podem até usar dos mesmos ingredientes/palavras do analista, porém,
-            só fazem sujeira (e antes fosse fácil de limpar como os do bolo).
-            Pois, não sabem ouvir tecnicamente, não conhecem a ‘alquimia’ que
-            transforma o psiquismo. Desconhecem o momento e a medida adequada
-            das palavras no momento exato que a transformação pode ocorrer –
-            experimentem errar o tempo e a medida do amido de milho.
-          </p>
+            <p className="text-base font-normal">
+              Imaginem: uma criança na cozinha imita os mesmos movimentos de sua
+              mãe que prepara o bolo. Manuseia farinha, fermento, cacau,
+              morango, açúcar, água, etc. Se não estiver sob orientação, a
+              criança faz apenas sujeira com os ingredientes. A mãe os
+              transforma num bolo, pois, domina a ‘alquimia’ - as medidas e o
+              tempo adequado para a transformação. Os palpiteiros e conselheiros
+              são como essa criança, podem até usar dos mesmos
+              ingredientes/palavras do analista, porém, só fazem sujeira (e
+              antes fosse fácil de limpar como os do bolo). Pois, não sabem
+              ouvir tecnicamente, não conhecem a ‘alquimia’ que transforma o
+              psiquismo. Desconhecem o momento e a medida adequada das palavras
+              no momento exato que a transformação pode ocorrer – experimentem
+              errar o tempo e a medida do amido de milho.
+            </p>
+          </div>
         </div>
       </div>
     </section>
