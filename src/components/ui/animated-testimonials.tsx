ced120fb3eb7eee0ee02d59815ef575a71abe267
= <giovanedaniell@gmail.com>
2025-04-04 19:06:33 -0300

    refactor: remove Image and required name from testimonial

--- src/components/ui/animated-testimonials.tsx
+++ src/components/ui/animated-testimonials.tsx
@@ -1,15 +1,13 @@
 "use client";
 
 import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
-import { AnimatePresence, motion } from "motion/react";
-import Image from "next/image";
+import { motion } from "motion/react";
 import { useEffect, useState } from "react";
 
 type Testimonial = {
   quote: string;
-  name: string;
+  name?: string;
   designation: string;
-  src: string;
 };
 export const AnimatedTestimonials = ({
   testimonials,
@@ -43,55 +41,8 @@ export const AnimatedTestimonials = ({
     return Math.floor(Math.random() * 21) - 10;
   };
   return (
-    <div className="mx-auto max-w-sm px-4 py-20 font-sans antialiased md:max-w-4xl md:px-8 lg:px-12">
-      <div className="relative grid grid-cols-1 gap-20 md:grid-cols-2">
-        <div>
-          <div className="relative h-80 w-full">
-            <AnimatePresence>
-              {testimonials.map((testimonial, index) => (
-                <motion.div
-                  key={testimonial.src}
-                  initial={{
-                    opacity: 0,
-                    scale: 0.9,
-                    z: -100,
-                    rotate: randomRotateY(),
-                  }}
-                  animate={{
-                    opacity: isActive(index) ? 1 : 0.7,
-                    scale: isActive(index) ? 1 : 0.95,
-                    z: isActive(index) ? 0 : -100,
-                    rotate: isActive(index) ? 0 : randomRotateY(),
-                    zIndex: isActive(index)
-                      ? 40
-                      : testimonials.length + 2 - index,
-                    y: isActive(index) ? [0, -80, 0] : 0,
-                  }}
-                  exit={{
-                    opacity: 0,
-                    scale: 0.9,
-                    z: 100,
-                    rotate: randomRotateY(),
-                  }}
-                  transition={{
-                    duration: 0.4,
-                    ease: "easeInOut",
-                  }}
-                  className="absolute inset-0 origin-bottom"
-                >
-                  <Image
-                    src={testimonial.src}
-                    alt={testimonial.name}
-                    width={500}
-                    height={500}
-                    draggable={false}
-                    className="h-full w-full rounded-3xl object-cover object-center"
-                  />
-                </motion.div>
-              ))}
-            </AnimatePresence>
-          </div>
-        </div>
+    <div className="mx-auto w-full py-4 font-sans antialiased md:max-w-4xl px-2">
+      <div className="relative grid grid-cols-1 gap-20">
         <div className="flex flex-col justify-between py-4">
           <motion.div
             key={active}
@@ -112,13 +63,15 @@ export const AnimatedTestimonials = ({
               ease: "easeInOut",
             }}
           >
-            <h3 className="text-2xl font-bold text-foreground/80">
-              {testimonials[active].name}
-            </h3>
+            {testimonials[active].name && (
+              <h3 className="text-2xl font-bold text-foreground/80">
+                {testimonials[active].name}
+              </h3>
+            )}
             <p className="text-sm text-foreground/50">
               {testimonials[active].designation}
             </p>
-            <motion.p className="mt-8 text-lg text-foreground">
+            <motion.p className="mt-2 text-lg text-foreground h-20">
               {testimonials[active].quote.split(" ").map((word, index) => (
                 <motion.span
                   key={index}
@@ -144,7 +97,8 @@ export const AnimatedTestimonials = ({
               ))}
             </motion.p>
           </motion.div>
-          <div className="flex gap-4 pt-12 md:pt-0">
+
+          <div className="flex gap-4 mt-4 items-center justify-end">
             <button
               onClick={handlePrev}
               className="group/button flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-gray-100 dark:bg-neutral-800"
--- src/static/testimonials.ts
+++ src/static/testimonials.ts
@@ -1,37 +1,82 @@
-export const testimonials = [
+const colleagues = [
+  {
+    quote:
+      "O cara é bom. Pode confiar.",
+    name: "Steve Jobs",
+    designation: "CEO na Apple"
+  },
+  {
+    quote:
+      "O bicho é bom memo. Diferenciado. Rapido virado na desgraça.",
+    name: "Bill Gates",
+    designation: "CEO na Microsoft"
+  },
+  {
+    quote:
+      "Ele é diferentão. O cara é bom. Ajudou a decidir o nome do meu filho.",
+    name: "Elon Musk",
+    designation: "CEO na SpaceX"
+  }
+];
+
+const clients = [
+  {
+    quote:
+      "Parabéns por desempenhar um papel tão crucial. Você salva vidas, proporciona ajuda e guia para novas oportunidades. Sua crença em mim foi transformadora; hoje, sou uma pessoa melhor graças a isso.",
+    designation: "Cliente anônimo"
+  },
+  {
+    quote:
+      "Nada a sugerir, apenas agradecer pelos cuidados que deixou em mim. Muito obrigada.",
+    designation: "Cliente anônimo"
+  },
+  {
+    quote:
+      "Helder Barbosa é uma excelente pessoa, o que o torna um excelente profissional. E vice-versa.",
+    designation: "Cliente anônimo"
+  },
+  {
+    quote:
+      "Através da psicoterapia tenho menos dias nublados comigo mesma e tenho começado a criar a coragem de colocar meus limites.",
+    designation: "Cliente anônimo"
+  },
+  {
+    quote:
+      "Bom aqui fica o agradecimento por ser um profissional tão sensível e capacitado para lidar com as emoções e questões que muitas vezes são tão pertubadoras. Obrigada!!!",
+    designation: "Cliente anônimo"
+  },
+];
+
+const students = [
   {
     quote:
       "A atenção aos detalhes e os recursos inovadores transformaram completamente nosso fluxo de trabalho. Isso é exatamente o que estávamos procurando.",
-    name: "Sarah Chen",
-    designation: "Gerente de Produto na TechFlow",
-    src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    designation: "Participante de estudos e supervisões anônimo"
   },
   {
     quote:
       "A implementação foi tranquila e os resultados superaram nossas expectativas. A flexibilidade da plataforma é notável.",
-    name: "Michael Rodriguez",
-    designation: "CTO na InnovateSphere",
-    src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    designation: "Participante de estudos e supervisões anônimo"
   },
   {
     quote:
       "Esta solução melhorou significativamente a produtividade da nossa equipe. A interface intuitiva torna tarefas complexas simples.",
-    name: "Emily Watson",
-    designation: "Diretora de Operações na CloudScale",
-    src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    designation: "Participante de estudos e supervisões anônimo"
   },
   {
     quote:
       "Suporte excepcional e recursos robustos. É raro encontrar um produto que cumpra todas as suas promessas.",
-    name: "James Kim",
-    designation: "Líder de Engenharia na DataPro",
-    src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    designation: "Participante de estudos e supervisões anônimo"
   },
   {
     quote:
       "A escalabilidade e o desempenho foram transformadores para nossa organização. Altamente recomendado para qualquer empresa em crescimento.",
-    name: "Lisa Thompson",
-    designation: "VP de Tecnologia na FutureNet",
-    src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    designation: "Participante de estudos e supervisões anônimo"
   },
 ];
+
+export const testimonials = {
+  colleagues,
+  clients,
+  students,
+}
