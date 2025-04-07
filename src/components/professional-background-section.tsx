cfe01d99642664433c82afc4ebc70c732128a8d5
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-04-07 18:33:11 -0300

    feat: add timeline usage on professional background section

--- src/components/professional-background-section.tsx
+++ src/components/professional-background-section.tsx
@@ -1,37 +1,57 @@
-"use client";
-
-import { professionalBackground } from "./static/professional-background";
+import {
+  studyGroupsData,
+  timelineProfessionalBackground,
+} from "./static/professional-background";
+import { Button } from "./ui/button";
+import * as Card from "./ui/card";
+import { Timeline } from "./ui/timeline";
 
 export function ProfessionalBackgroundSection() {
   return (
     <section
       id="professional-background"
-      className="w-full flex flex-col items-center justify-center"
+      className="container w-full flex flex-col items-center justify-center"
     >
       <h1 className="text-4xl font-bold mt-20 pb-10">Formações e Dedicações</h1>
 
-      <div className="w-full flex flex-col items-center justify-evenly text-white bg-center bg-background bg-no-repeat bg-cover bg-blend-color bg-opacity-50 border-b pb-10">
-        <div className="w-10/12 h-fit flex justify-center items-center text-center flex-col">
-          <ol className="grid grid-cols-1 gap-y-10">
-            {professionalBackground.map((item) => (
-              <li
-                key={item.text}
-                className="flex flex-row items-center justify-center text-md gap-2 h-fit"
+      <div className="grid grid-cols-1 gap-16 items-center text-white bg-center bg-background bg-no-repeat bg-cover bg-blend-color bg-opacity-50 border-b pb-10">
+        <div className="w-[55vw] h-fit flex justify-center items-center flex-col">
+          <div className="grid grid-cols-2 gap-4">
+            {studyGroupsData.map((studyGroup) => (
+              <Card.Card
+                key={studyGroup.text}
+                className="border border-white/10 shadow-sm text-white bg-card/10"
               >
-                <p className="text-center font-medium text-lg">{item.text}</p>
+                <Card.CardContent className="flex flex-col justify-center items-center gap-3 px-14">
+                  <p className="text-center text-base h-12">
+                    {studyGroup.text}
+                  </p>
 
-                {item.redirectUrl && (
-                  <a
-                    href={item.redirectUrl}
-                    target="_blank"
-                    className="flex items-center text-lg font-bold underline"
-                  >
-                    Saiba Mais
-                  </a>
-                )}
-              </li>
+                  <Button className="w-fit" variant="secondary" asChild>
+                    <a
+                      href={studyGroup.redirectUrl}
+                      target="_blank"
+                      className="flex items-center text-lg font-bold"
+                    >
+                      Saiba Mais
+                    </a>
+                  </Button>
+                </Card.CardContent>
+              </Card.Card>
             ))}
-          </ol>
+          </div>
+
+          <div>
+            <Timeline data={timelineProfessionalBackground} />
+          </div>
+
+          <span className="text-base text-justify">
+            Além da prática clínica e estudos cotidianos que a fundamentam, e a
+            experiência com supervisão a psicoterapeutas, dedica-se também à
+            coordenação do GEPO como coordenador geral e também às suas
+            respectivas atividades, bem como ministra dois Grupos de Estudos
+            através desta instituição
+          </span>
         </div>
       </div>
     </section>
--- src/components/static/professional-background.ts
+++ src/components/static/professional-background.ts
@@ -1,4 +1,6 @@
-export const professionalBackground = [
+import { TimelineData } from "../ui/timeline";
+
+export const studyGroupsData = [
   {
     text: `Grupo de Estudos “Introdução ao pensamento clínico de Donald Woods Winnicott: estudos da obra”.`,
     redirectUrl: "https://forms.gle/t3ySLCKFfbLNtBjD7",
@@ -7,31 +9,52 @@ export const professionalBackground = [
     text: `Grupo de Estudos “A metapsicologia freudiana e kleiniana”.`,
     redirectUrl: "https://forms.gle/J1ZyY9kqUaB8crE97",
   },
+];
+
+export const timelineProfessionalBackground: TimelineData[] = [
   {
-    text: `Psicólogo Clínico pela UniFio [2006 a 2010]`,
+    time: "Desde 2024",
+    title:
+      "Membro do Grupo de Estudos de Winnicott: Playground Psicanalítico - ministrado por Ricardo Telles de Deus, entre outros cursos de aprimoramento e extensão",
+    description: "...e mais de 15 anos de experiência com análise pessoal;",
   },
   {
-    text: `Especialização em Psicologia Clínica Psicanalítica pela Universidade Estadual de Londrina, PR (UEL) [2012 a 2013];`,
+    time: "2023",
+    title:
+      "Cursou “Para além da Contratransferência: o analista implicado” com Gina Tamburrino pelo ISS",
   },
   {
-    text: `Formação em Psicanálise pelo Instituto Sedes Sapientiae (ISS) – São Paulo, SP [2015 a 2019];`,
+    time: "2020 a 2023",
+    title:
+      "Formação em Psicanálise pelo Instituto de Psicanálise da Sociedade Brasileira de Psicanálise de São Paulo (SBPSP)",
   },
   {
-    text: `Formação em Psicanálise pelo Instituto de Psicanálise da Sociedade Brasileira de Psicanálise de São Paulo (SBPSP) [2020 a 2023];`,
+    time: "2020 a 2023",
+    title:
+      "Formação em Psicoterapia Breve Psicanalítica pelo Instituto de Psicanálise da Sociedade Brasileira de Psicanálise de São Paulo (SBPSP)",
   },
   {
-    text: `Cursou “Para além da Contratransferência: o analista implicado” com Gina Tamburrino pelo ISS [2023];`,
+    time: "Desde 2020",
+    title:
+      "Coordenador do Grupo de Estudos de Psicanálise de Ourinhos e região (GEPO)",
   },
   {
-    text: `Formação em Psicoterapia Breve Psicanalítica pelo Instituto de Psicanálise da Sociedade Brasileira de Psicanálise de São Paulo (SBPSP) [2020 a 2023];`,
+    time: "2015 a 2019",
+    title:
+      "Formação em Psicanálise pelo Instituto Sedes Sapientiae (ISS) – São Paulo, SP",
   },
   {
-    text: `Membro do Grupo de Estudos de Psicanálise de Ourinhos e região (GEPO) desde 2009 e coordenador desde 2020;`,
+    time: "2012 a 2013",
+    title:
+      "Especialização em Psicologia Clínica Psicanalítica pela Universidade Estadual de Londrina, PR (UEL)",
   },
   {
-    text: `Membro do Grupo de Estudos de Winnicott: Playground Psicanalítico - ministrado por Ricardo Telles de Deus [desde 2024], entre outros cursos de aprimoramento e extensão, e mais de 15 anos de experiência com análise pessoal;`,
+    time: "Desde 2009",
+    title:
+      "Membro do Grupo de Estudos de Psicanálise de Ourinhos e região (GEPO)",
   },
   {
-    text: `Além da prática clínica e estudos cotidianos que a fundamentam, e a experiência com supervisão a psicoterapeutas, dedica-se também à coordenação do GEPO como coordenador geral e também às suas respectivas atividades, bem como ministra dois Grupos de Estudos através desta instituição`,
-  }
+    time: "2006 a 2010",
+    title: "Psicólogo Clínico pela UniFio",
+  },
 ];
