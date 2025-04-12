bb9506ae878c46c5d9cbfdf335b1b3f43b9dcdb4
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-04-12 11:52:01 -0300

    fix: page scroll with sidebar open

--- src/components/navigation/sidebar.tsx
+++ src/components/navigation/sidebar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Portal } from "@radix-ui/react-portal";
 import { MenuIcon, XIcon } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 import React, { ComponentProps } from "react";
@@ -25,33 +26,35 @@ const overlay = {
 
 function Sidebar({ open, onClose, children }: SidebarProps) {
   return (
-    <AnimatePresence>
-      {open && (
-        <React.Fragment>
-          <motion.div
-            className="fixed inset-0 bg-black/50 h-screen z-30 overflow-hidden"
-            initial="closed"
-            animate="open"
-            exit="closed"
-            variants={overlay}
-            onClick={onClose}
-          />
+    <Portal>
+      <AnimatePresence>
+        {open && (
+          <React.Fragment>
+            <motion.div
+              className="fixed inset-0 bg-black/50 h-screen z-40"
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={overlay}
+              onClick={onClose}
+            />
 
-          <motion.aside
-            initial="closed"
-            animate="open"
-            exit="closed"
-            variants={sidebar}
-            transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            className="flex flex-col justify-center fixed top-0 left-0 h-screen w-[75dvw] bg-background z-40 p-6 border-r border-card/10 shadow-lg"
-          >
-            {children}
+            <motion.aside
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={sidebar}
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              className="flex flex-col justify-center fixed top-0 left-0 h-screen w-[75dvw] bg-background z-50 p-6 border-r border-card/10 shadow-lg"
+            >
+              {children}
 
-            <SidebarClose onClick={onClose} />
-          </motion.aside>
-        </React.Fragment>
-      )}
-    </AnimatePresence>
+              <SidebarClose onClick={onClose} />
+            </motion.aside>
+          </React.Fragment>
+        )}
+      </AnimatePresence>
+    </Portal>
   );
 }
 
